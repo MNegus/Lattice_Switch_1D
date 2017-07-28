7e5bc607db2cb979a694051a1b91f62ec73959f5
Michael Negus <michaelnegus1@gmail.com>
2017-07-29 00:37:12 +0100

    More changes on the creators

--- prototype_rundir/job_creator.sh
+++ prototype_rundir/job_creator.sh
@@ -7,24 +7,33 @@ TEMP_MIN=0.1;
 TEMP_MAX=1.0;
 TEMP_INC=0.1;
 
+WORK_DIRECTORY="/storage/molsim/phuhzr/storage/molsim/phuhzr/Lattice_Switch_1D"
 
 
 for POTENTIAL_NAME in "KT" "QUARTIC" "DIFF_WIDTH"
 do
+	DATA_DIRECTORY="/home/theory/phuhzr/Documents/URSS/data_storage/"$POTENTIAL_NAME;
 	for JOB_NO in $(seq 1 10)
 	do
 		TIME_MIN=$(python -c "print $OVERALL_TIME_MIN + ($JOB_NO - 1) * 10 * $TIME_INC");
 		TIME_MAX=$(python -c "print $TIME_MIN + 9 * $TIME_INC");
 
-		jobfilename="runjob_"$POTENTIAL_NAME"_"$JOB_NO".sh";
+		jobfilename="runjob_"$POTENTIAL_NAME"_"$JOB_NO".pbs";
 		echo "#!/bin/bash">$jobfilename;
-		echo "POTENTIAL_NAME="$POTENTIAL_NAME>>$jobfilename;
-		echo "JOB_NO="$JOB_NO>>$jobfilename;
-		echo "TIME_INC="$TIME_INC>>$jobfilename;
-		echo "TEMP_INC="$TEMP_INC>>$jobfilename;
+		echo "#PBS -l nodes=1:ppn=1,mem=256mb,walltime=08:00:00">>$jobfilename;
+		echo "#PBS -V">>$jobfilename;
 		echo " ">>$jobfilename;
-		echo "TIME_MIN="$TIME_MIN>>$jobfilename;
-		echo "TIME_MAX="$TIME_MAX>>$jobfilename;
+		echo "WORK_DIRECTORY="$WORK_DIRECTORY";">>$jobfilename;
+		echo "DATA_DIRECTORY="$DATA_DIRECTORY";">>$jobfilename;
+		echo "cd $WORK_DIRECTORY;">>$jobfilename;
+		echo " ">>$jobfilename;
+		echo "POTENTIAL_NAME="$POTENTIAL_NAME";">>$jobfilename;
+		echo "JOB_NO="$JOB_NO";">>$jobfilename;
+		echo "TIME_INC="$TIME_INC";">>$jobfilename;
+		echo "TEMP_INC="$TEMP_INC";">>$jobfilename;
+		echo " ">>$jobfilename;
+		echo "TIME_MIN="$TIME_MIN";">>$jobfilename;
+		echo "TIME_MAX="$TIME_MAX";">>$jobfilename;
 		cat bottom_half.txt>>$jobfilename;
 	done
 done
