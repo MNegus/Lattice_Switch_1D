731bc4908b68a63d8da9ea437b71d7bac1369f0f
Michael Negus <michaelnegus1@gmail.com>
2017-08-02 11:46:36 +0100

    Changed so jobs run longer and do fewer runs each time

--- prototype_rundir/job_creator.sh
+++ prototype_rundir/job_creator.sh
@@ -13,14 +13,14 @@ WORK_DIRECTORY="/storage/molsim/phuhzr/Lattice_Switch_1D"
 for POTENTIAL_NAME in "KT" "QUARTIC" "DIFF_WIDTH"
 do
 	DATA_DIRECTORY="/home/theory/phuhzr/Documents/URSS/data_storage/"$POTENTIAL_NAME;
-	for JOB_NO in $(seq 1 10)
+	for JOB_NO in $(seq 1 25)
 	do
-		TIME_MIN=$(python -c "print $OVERALL_TIME_MIN + ($JOB_NO - 1) * 10 * $TIME_INC");
-		TIME_MAX=$(python -c "print $TIME_MIN + 9 * $TIME_INC");
+		TIME_MIN=$(python -c "print $OVERALL_TIME_MIN + ($JOB_NO - 1) * 4 * $TIME_INC");
+		TIME_MAX=$(python -c "print $TIME_MIN + 3 * $TIME_INC");
 
 		jobfilename="runjob_"$POTENTIAL_NAME"_"$JOB_NO".pbs";
 		echo "#!/bin/bash">$jobfilename;
-		echo "#PBS -l nodes=1:ppn=1,mem=256mb,walltime=08:00:00">>$jobfilename;
+		echo "#PBS -l nodes=1:ppn=1,mem=256mb,walltime=12:00:00">>$jobfilename;
 		echo "#PBS -V">>$jobfilename;
 		echo " ">>$jobfilename;
 		echo "WORK_DIRECTORY="$WORK_DIRECTORY";">>$jobfilename;
