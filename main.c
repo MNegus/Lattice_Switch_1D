9f3fbc967944a93ad145d98fc4cdf272fefe3c6c
Michael Negus <michaelnegus1@gmail.com>
2017-08-02 14:48:40 +0100

    Mostly removing functions as they now appear in the newly created header files

--- main.c
+++ main.c
@@ -3,31 +3,11 @@
 #include <time.h>
 #include <math.h>
 #include <string.h>
+#include "Parameters.h"
 #include "Potentials.h"
+#include "Baoab.h"
 
 #define PI 3.14159265358979323846264338327
-
-/* Structure to store relevant parameters */
-struct parameters{
-	// Numerical parameters
-	char potential_name[10];
-	long tot_timesteps;
-	double timestep;
-	int start_well;
-	int switch_regularity;
-	int write_regularity;
-	
-	// Physical parameters
-	double kT;
-	double mass;
-	double shift_value;
-	double minima[2];
-
-	PotentialFun Poten;
-	PotentialFun Poten_shifted;
-	PotentialFun Poten_deriv;
-};
-typedef struct parameters parameters;
 
 
 // Returns the x-position of a particle in space given its displacement from the current well minima
@@ -63,11 +43,6 @@ double min(double x1, double x2){
 	}
 }
 
-double BAOAB_limit(double x, parameters *params, PotentialFun Poten_deriv, double R[]){
-	return x - params->timestep * (*Poten_deriv)(x) / params->mass +\
-		sqrt(0.5 * params->kT * params->timestep / params->mass) * (R[0] + R[1]);
-}
-
 // Calculates the free energy different between states in the two wells of a given potential function
 void create_energy_diff_data(parameters *params, char *outputfilename){
 	srand ( time(NULL) ); // Seeds the random number generators
@@ -98,7 +73,7 @@ void create_energy_diff_data(parameters *params, char *outputfilename){
 		}
 
 		// BAOAB step
-		x = BAOAB_limit(x, params, params->Poten_deriv, R); // Changes x according to the BAOAB limit method
+		x = BAOAB_limit(x, params, R); // Changes x according to the BAOAB limit method
 		R[0] = R[1]; // Current value of R becomes the next one
 		box_muller_rand(normal_dist);
 		R[1] = normal_dist[0]; // Next value for R is drawn from a normal distribution
@@ -132,72 +107,6 @@ void create_energy_diff_data(parameters *params, char *outputfilename){
 			}
 		}
 	}
-}
-
-/* Function to use fscanf to read strings from a line in an input file */
-void read_char(FILE *input_file, char *char_value){
-    /* Attempts to read line of input file */
-    if (fscanf(input_file, "%s", char_value) != 1) {
-        printf("Failed to read parameter\n");
-        exit(1);
-    }
-}
-
-/* Function to use fscanf to read doubles from a line in an input file */
-void read_double(FILE *input_file, double *double_value){
-    /* Attempts to read line of input file */
-    if (fscanf(input_file, "%lf", double_value) != 1) {
-        printf("Failed to read parameter\n");
-        exit(1);
-    }
-}
-
-
-/* Function to use fscanf to read longs from a line in an input file */
-void read_long(FILE *input_file, long *long_value){
-    /* Attempts to read line of input file */
-    if (fscanf(input_file, "%ld", long_value) != 1) {
-        printf("Failed to read parameter\n");
-        exit(1);
-    }
-}
-
-// Function to use fscanf to read ints from a line in an input file
-void read_int(FILE *input_file, int *int_value){
-	/* Attempts to read line of input file */
-    if (fscanf(input_file, "%d", int_value) != 1) {
-        printf("Failed to read parameter\n");
-        exit(1);
-    }
-}
-
-// Reads an input file to store the parameters
-void store_parameters(parameters *params, char *input_filename){
-	FILE *input_file = fopen(input_filename, "r");
-	if (input_filename == NULL){
-		printf("Failed to open input file \n");
-		exit(1);
-	}
-	read_char(input_file,   params->potential_name);
-	read_long(input_file,   &params->tot_timesteps);
-	read_double(input_file, &params->timestep);
-	read_int(input_file,    &params->start_well);
-	read_int(input_file,    &params->switch_regularity);
-	read_int(input_file,    &params->write_regularity);
-	read_double(input_file, &params->kT);
-	read_double(input_file, &params->mass);
-
-	double const_arr[] = {0, 0, 0}; // Array for constants
-	PotentialFun func_arr[] = {0, 0, 0}; // Array for potential functions 
-	U_selector(const_arr, func_arr, params->potential_name); // Fills the constant and function arrays
-
-	params->Poten = func_arr[0];
-	params->Poten_shifted = func_arr[1];
-	params->Poten_deriv = func_arr[2];
-
-	params->minima[0] = const_arr[0]; // x-coordinates of the minima of the wells
-	params->minima[1] = const_arr[1];
-	params->shift_value = const_arr[2]; // The amount the right minima has been shifted upwards
 }
 
 void calculate_energy_diff(double ret_arr[2], char *output_filename, double sample_portion, int sample_regularity){
