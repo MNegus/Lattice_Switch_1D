7e6877ba601b0f0bcfc7e4860d34c3187de09431
Michael Negus <michaelnegus1@gmail.com>
2017-08-09 13:35:12 +0100

    Changed method of getting the mean and standard error. Made saving bins optional, and only save the last lot of bins.

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ double well_dis(parameters *params, int well, double x_position) {
 }
 
 // Attempts a lattice switch from a given position in a well
-double lattice_switch(double x, int *cur_well, long stepno, long no_left, char *outputfilename, parameters *params) {
+double lattice_switch(double x, int *cur_well, parameters *params) {
     double dis = well_dis(params, *cur_well, x); // Displacement from the current well
     int oth_well = (*cur_well + 1) % 2; // Other well
     double diff_poten = (*params->Poten_shifted)(x_pos(params, oth_well, dis)) - \
@@ -27,13 +27,6 @@ double lattice_switch(double x, int *cur_well, long stepno, long no_left, char *
     if (genrand_real1() < min(1, exp(-diff_poten / params->kT))) {
         *cur_well = oth_well;
         x = x_pos(params, *cur_well, dis);
-    }
-
-    if ((stepno / params->switch_regularity) % params->write_regularity == 0) {
-        double energy_diff = -params->kT * log((double) (no_left) / (stepno - no_left)) + params->shift_value;
-        FILE *outputfile = fopen(outputfilename, "a");
-        fprintf(outputfile, "%g\n", energy_diff);
-        fclose(outputfile);
     }
     return x;
 }
@@ -54,21 +47,20 @@ void add_to_bins(double x, long *bins, parameters *params) {
 }
 
 // Calculates the free energy different between states in the two wells of a given potential function
-void create_energy_diff_data(parameters *params, char *energy_diff_filename, char *bins_filename) {
-    init_genrand(time(NULL)); // Seeds the random number generators
-
+double calc_energy_difference(parameters *params, int savebins, char *bins_filename) {
     double x = x_pos(params, params->start_well, 0); // x-position initially at the bottom of the starting well
     long no_left = 0; // Number of timesteps that the particle is in the left well
     int cur_well = params->start_well; // Indicates which well the particle is in (0 is left well, 1 is right well)
-    long *bins = malloc(sizeof(long) * (params->nobins)); // Array to store number of times each bin has been visited
 
-    for (long j = 0; j < params->nobins; j++) {
-        bins[j] = 0;
+    long *bins; // Array for storing amount of times the walker has visited each bin
+
+    if (savebins) {
+        bins = malloc(sizeof(long) * (params->nobins)); // Array to store number of times each bin has been visited
+        for (long j = 0; j < params->nobins; j++) bins[j] = 0;
+
+        // Removes existing bin files
+        remove(bins_filename);
     }
-
-    // Removes existing output files
-    remove(energy_diff_filename);
-    remove(bins_filename);
 
     DynamicsFun DynFun = Dynamics_selector(params->dynamics_type); // Returns the desired dynamics function
 
@@ -81,7 +73,8 @@ void create_energy_diff_data(parameters *params, char *energy_diff_filename, cha
 
     // Perform lattice switching method
     for (long stepno = 1; stepno < params->tot_steps; stepno++) {
-        add_to_bins(x, bins, params);
+
+        if (savebins) add_to_bins(x, bins, params);
 
         if (cur_well == 0) {
             // Indicates that the particle was in the left well
@@ -105,120 +98,81 @@ void create_energy_diff_data(parameters *params, char *energy_diff_filename, cha
 
         // Attempts a lattice switch
         if (stepno % params->switch_regularity == 0) {
-            x = lattice_switch(x, &cur_well, stepno, no_left, energy_diff_filename, params);
+            x = lattice_switch(x, &cur_well, params);
         }
     }
 
-    FILE *bins_file = fopen(bins_filename, "w");
-    for (long j = 1; j <= params->nobins; j++) {
-        double bin_x_pos = params->x_min + j * params->bin_width;
-        fprintf(bins_file, "%g, %ld\n", bin_x_pos, bins[j - 1]);
-    }
-    fclose(bins_file);
-//    for (long j = 0; j < params->nobins; j++){
-//        printf("%ld\n", bins[j]);
-//    }
-    free(bins);
-
-}
-
-// Calculates the mean and standard error of the data outputted from a lattice switching procedure
-void calculate_energy_diff(double ret_arr[2], char *output_filename, double sample_portion, int sample_regularity) {
-    long no_lines = 0; // Number of lines in file
-
-    // Counts the number of lines in the output file
-    char ch;
-    FILE *output_file = fopen(output_filename, "r");
-    while ((ch = fgetc(output_file)) != EOF) {
-        if (ch == '\n') {
-            no_lines++;
+    if (savebins) {
+        FILE *bins_file = fopen(bins_filename, "w"); // Data file to store bin data
+        for (long j = 1; j <= params->nobins; j++) {
+            // Fills data file with bin positions and how many hits they have
+            double bin_x_pos = params->x_min + j * params->bin_width;
+            fprintf(bins_file, "%g, %ld\n", bin_x_pos, bins[j - 1]);
         }
-    }
-    fclose(output_file);
+        fclose(bins_file);
 
-    // Calculates the first line of the block of data we want to keep
-    long start_line = (long) (no_lines * (1 - sample_portion));
-    while (start_line % sample_regularity != 0) {
-        // Decreases the start line until it is divisible by the sample regularity
-        start_line--;
+        free(bins);
     }
 
-    long arr_length =
-            (long) ((double) (no_lines - start_line) / sample_regularity) + 1; // Size of array to store all the data
-    double *data_arr = malloc(sizeof(double) * (arr_length));
-
-    double line_val; // Value read in by each line
-    double mean = 0; // Mean of data
-    output_file = fopen(output_filename, "r");
-    long line = 1; // Indicates which line of the file we are on
-
-    long arr_pos = 0; // Position in the array we are currently reading into
-
-    // Reads values from lines in the file to the variable line_val
-    while (fscanf(output_file, "%lf", &line_val) == 1) {
-        if ((line >= start_line) && (line % sample_regularity == 0)) {
-            // Saves the data once we are at the portion of the file we want, and at the chosen regularity
-            data_arr[arr_pos] = line_val;
-            mean += line_val;
-            arr_pos++;
-        }
-        line++;
-    }
-    fclose(output_file);
-
-    mean = mean / arr_length;
-    ret_arr[0] = mean;
-
-    // Calculates standard error
-    double std_error = 0;
-    for (long j = 0; j < arr_length; j++) {
-        std_error += (data_arr[j] - mean) * (data_arr[j] - mean);
-    }
-
-    std_error = sqrt(std_error) / arr_length;
-    ret_arr[1] = std_error;
-
-    free(data_arr);
+    return -params->kT * log((double) (no_left) / (params->tot_steps - no_left)) + params->shift_value;
 }
 
 int main(int argc, char **argv) {
     char *input_filename = argv[1]; // Name of the parameter input file
-    char *output_filename = argv[2]; // Name of the file to output the free energy at every step
-    char *datastore_filename = argv[3]; // Name of the file to store final calculated data
+    char *datastore_filename = argv[2]; // Name of the file to store final calculated data
+    char *bins_filename = argv[3]; // Name of the bin output file
+    char *seed_str = argv[4]; // Seed of the simulation
+
+    char *ptr;
+    unsigned long seed = (unsigned long) strtol(seed_str, &ptr, 10);
+
+    // ///////////////////////////////
+    // CHANGE WHEN ACTUALLY SIMULATING
+    seed = (unsigned long) time(NULL);
+    // ///////////////////////////////
+    
+    int savebins = 1; // Variable for indicating if the data for bins will be saved or not
+    if (strcmp(bins_filename, "NOBINS") == 0) savebins = 0;
+
+
+    init_genrand(seed); // Seeds the random number generators
 
     parameters params; // Struct for storing parameters
     store_parameters(&params, input_filename); // Fills the parameters struct with data from input file
 
-    double mean_of_simulations = 0;
-    double std_error_of_simulations = 0;
+    double mean_energy_diff = 0;
+    double std_error = 0;
 
-    /* Creates file names for outputting the energies and the bins */
-    char energy_diff_filename[50];
-    strcpy(energy_diff_filename, output_filename);
-    strcat(energy_diff_filename, "_energies.csv");
-
-    char bins_filename[50];
-    strcpy(bins_filename, output_filename);
-    strcat(bins_filename, "_bins.csv");
+    double energy_differences[10];
 
     // Runs the procedure 10 times to calculate an average
-    for (int i = 0; i < 10; i++) {
-        create_energy_diff_data(&params, energy_diff_filename,
-                                bins_filename); // Runs the lattice switch procedure to create data in file
-        double ret_arr[2] = {0, 0}; // Array to store average and standard error
-        calculate_energy_diff(ret_arr, energy_diff_filename, 0.01,
-                              100); // Calculates mean and standard error of the procedure
-        mean_of_simulations += ret_arr[0];
-        std_error_of_simulations += ret_arr[1] * ret_arr[1];
+    for (int i = 0; i < 9; i++) {
+        energy_differences[i] = calc_energy_difference(&params, 0,
+                                                       bins_filename); // Runs the lattice switch procedure to create data in file
+        mean_energy_diff += energy_differences[i];
     }
 
-    mean_of_simulations = mean_of_simulations / 10;
-    std_error_of_simulations = sqrt(std_error_of_simulations) / 10;
+    energy_differences[10] = calc_energy_difference(&params, savebins,
+                                                    bins_filename); // Runs the lattice switch procedure to create data in file
+    mean_energy_diff += energy_differences[10];
+
+    mean_energy_diff /= 10;
+    for (int i = 0; i < 10; i++) {
+        std_error += (energy_differences[i] - mean_energy_diff) * (energy_differences[i] - mean_energy_diff);
+    }
+
+    std_error = sqrt(std_error / 10);
 
     FILE *datastore_file = fopen(datastore_filename, "a");
     // Appends the data file with the mean and standard error, also listing the parameters associated with the run
-    fprintf(datastore_file, "%s, %ld, %lf, %lf, %lf, %g, %g\n", params.potential_name, params.tot_steps,
-            params.timestep, params.kT, params.mass, mean_of_simulations, std_error_of_simulations);
+    if (strcmp(params.dynamics_type, "BAOAB_LIMIT") == 0) {
+        fprintf(datastore_file, "%s, %s, %ld, %lf, %lf, %g, %g\n", params.potential_name, params.dynamics_type,
+                params.tot_steps, params.timestep, params.kT, mean_energy_diff, std_error);
+    } else if (strcmp(params.dynamics_type, "MONTE-CARLO") == 0) {
+        fprintf(datastore_file, "%s, %s, %ld, %lf, %lf, %g, %g\n", params.potential_name, params.dynamics_type,
+                params.tot_steps, params.jump_size, params.kT, mean_energy_diff, std_error);
+    }
+
     fclose(datastore_file);
     return 0;
 }
